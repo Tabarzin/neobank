c70a8625b07b09cba130e32d3153e3e3f2853dc1
Rustam <tabarzin@gmail.com>
2024-06-08 20:13:52 +0300

    feat: add aliases config

--- neobank/vite.config.ts
+++ neobank/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@App': path.resolve(__dirname, './src/App'),
+      '@configs': path.resolve(__dirname, './src/configs'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
   plugins: [react()],
-})
+});
