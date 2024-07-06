5bf8f095d6fdbdfaa1e680cf48d91d40639eac8a
Rustam <tabarzin@gmail.com>
2024-07-06 18:05:08 +0300

    feat: add tooltip

--- neobank/src/App/pages/CreditCard/CreditCardHero/CreditCardHero.tsx
+++ neobank/src/App/pages/CreditCard/CreditCardHero/CreditCardHero.tsx
@@ -2,6 +2,7 @@ import Button from '@components/Button/Button';
 import './CreditCardHero.scss';
 import { useNavigate } from 'react-router-dom';
 import creditCard from '@assets/images/credit-card_cardImage1.svg';
+import Tooltip from '@components/Tooltip/Tooltip';
 
 const CreditCardHero: React.FC = () => {
   const navigate = useNavigate();
@@ -22,15 +23,21 @@ const CreditCardHero: React.FC = () => {
           <div className="credit-card-conditions">
             <div className="conditions-item">
               <span className="conditions-item-m">Up to 160 days</span>
-              <span className="conditions-item-s">No percent</span>
+              <Tooltip text="When repaying the full debt up to 160 days.">
+                <span className="conditions-item-s">No percent</span>
+              </Tooltip>
             </div>
             <div className="conditions-item">
               <span className="conditions-item-m">Up to 600 000 </span>
-              <span className="conditions-item-s">Credit limit</span>
+              <Tooltip text="Over the limit will accrue percent">
+                <span className="conditions-item-s">Credit limit</span>
+              </Tooltip>
             </div>
             <div className="conditions-item">
               <span className="conditions-item-m">0 ₽</span>
-              <span className="conditions-item-s">Card service is free</span>
+              <Tooltip text="Promotion valid until December 31, 2024.">
+                <span className="conditions-item-s">Card service is free</span>
+              </Tooltip>
             </div>
           </div>
           <Button onClick={handleClick}>Apply for card</Button>
